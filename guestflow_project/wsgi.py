58b3ba340088ebe188fd01b604c2b1e722a45aca
Eva254-ke <evanszachariah36@gmail.com>
2025-07-03 10:39:03 +0300

    Auto run migrations on startup in wsgi.py

--- guestflow_project/wsgi.py
+++ guestflow_project/wsgi.py
@@ -1,6 +1,14 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guestflow_project.settings')
+django.setup()
+
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration error: {e}")
 
 application = get_wsgi_application()
