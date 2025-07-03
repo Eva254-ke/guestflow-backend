6f64e5340e6556b116793312c4e047cc63fd45e9
Eva254-ke <evanszachariah36@gmail.com>
2025-07-04 02:22:13 +0300

    Fix DailyRoomPriceSerializer: use SerializerMethodField for rate_used

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -17,6 +17,12 @@ class ExchangeRateSerializer(serializers.ModelSerializer):
         fields = ['id', 'from_currency', 'to_currency', 'rate', 'date']
 
 class DailyRoomPriceSerializer(serializers.ModelSerializer):
+    rate_used = serializers.SerializerMethodField()
+
     class Meta:
         model = DailyRoomPrice
         fields = ['id', 'room', 'date', 'price', 'rate_used']
+
+    def get_rate_used(self, obj):
+        # Assumes obj has a rate_used attribute or property set in the queryset or view
+        return getattr(obj, 'rate_used', None)
